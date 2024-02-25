ce0ce2337826f1f29a74fc5fcdc102cdf0d8180b
AzizBahloul <azizbahloul3@gmail.com>
2024-02-25 12:57:02 +0100

    update

---
+++ React Basics/week/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './calculator';
+
+function App() {
+  
+
+  return (
+    <>
+    <Calculator />
+    </>
+  );
+}
+
+export default App;
